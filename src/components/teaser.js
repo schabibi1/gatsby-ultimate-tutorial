f7b033ed8aa9cd703b7393b8fda2eefef75a9827
Patrick Odey <patrick@Patricks-MBP.lan>
2022-08-23 22:37:58 +0100

    grid component

--- src/components/teaser.js
+++ src/components/teaser.js
@@ -6,7 +6,6 @@ const Teaser = ({ blok }) => {
   return (
     <div className="teaser" {...storyblokEditable(blok)}>
       <h2 className="text-2xl mb-10" >
-      <img src={blok.image} alt="image" />
         {blok.headline}
       </h2>
     </div>
