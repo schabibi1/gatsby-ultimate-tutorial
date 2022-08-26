51823f2ab749b89dfd2dd47f41640514fe3b01f5
schabibi1 <arisagse@gmail.com>
2022-08-26 17:20:08 +0200

    rename navigation.js into header.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import { storyblokInit, apiPlugin } from "gatsby-source-storyblok"
 import Teaser from './teaser'
 import Grid from "./grid"
 import Feature from "./feature"
-import Navigation from "./navigation"
+import Header from "./header"
 import Footer from "./footer"
 import Config from "./config"
 import MenuLink from "./menuLink"
@@ -24,7 +24,7 @@ storyblokInit({
 const Layout = ({ children }) => {
   return (
     <div className="text-center">
-      <Navigation />
+      <Header />
       {children}
       <Footer />
     </div>
