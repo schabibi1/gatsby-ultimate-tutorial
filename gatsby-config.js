60208d93b9ea113792868d80e4360b940d48bb52
Patrick odey <patrickodey419@hmail.com>
2023-10-26 17:40:05 +0100

    upgraded to gatsby 5 and gatsby-source-storyblok 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@ require("dotenv").config({
 })
 
 module.exports = {
+  flags: {
+    PARTIAL_HYDRATION: true
+  },
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
