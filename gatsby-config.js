89341d1440667c2f081bd0febc077ef4f3b70196
Patrick odey <patrickodey419@hmail.com>
2023-12-12 16:12:42 +0100

    Part-5 final fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
-    `gatsby-plugin-postcss`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,23 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 
-// import { StoryblokComponent, storyblokEditable, useStoryblokState } from "gatsby-source-storyblok"
-import { StoryblokStory, useStoryblokState } from "gatsby-source-storyblok"
+import { StoryblokComponent, storyblokEditable, useStoryblokState } from "gatsby-source-storyblok"
 
 import Layout from "../components/layout"
 
 export default function Home({ data }) {
-  console.log(data)
-  if (typeof data.storyblokEntry.content === "string") data.storyblokEntry.content = JSON.parse(data.storyblokEntry.content);
-
   let story = data.storyblokEntry
   story = useStoryblokState(story, {
     resolveRelations: ["popular-articles.articles"],
   })
 
-  // const components = story.content.body.map(blok => (<StoryblokComponent blok={blok} key={blok._uid} />))
-  const components = story.content.body.map(blok => (<StoryblokStory story={blok} blok={blok} key={blok._uid} />))
+  const components = story.content.body.map(blok => (<StoryblokComponent blok={blok} key={blok._uid} />))
 
   return (
     <Layout>
+      <div {...storyblokEditable(story.content)}>
         {components}
+      </div>
     </Layout>
   )
 }
